d7a55f093f7e32740c6016cf56a1fea9b782cfb2
Subhan Hakim <subhanhakim07@gmail.com>
2024-11-13 21:22:47 +0700

    update

--- scripts/index.js
+++ scripts/index.js
@@ -10,11 +10,11 @@ function validateForm() {
   var password = document.getElementById("password").value;
 
   //validasi username dan password, username dan password bisa diganti sesuai keinginan
-  if (username === "sahsf" && password === "intel") {
+  if (username === "yulia" && password === "yulia123") {
     Swal.fire({
       icon: "success",
       title: "Login berhasil!",
-      text: "Selamat datang SHAFIRA",
+      text: "Selamat datang yulia",
       showConfirmButton: false,
       timer: 1500,
     }).then(function () {
